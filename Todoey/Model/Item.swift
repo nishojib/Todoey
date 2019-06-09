ace21377cb73c364dde98d98b8aa0953c439bf7b
Nurul Islam <nishojib@gmail.com>
2019-06-09 23:46:20 +0530

    Convert to MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by user155309 on 6/9/19.
+//  Copyright © 2019 user155309. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var done = false
+}
